e92b7ee9f93bad51233628e359fa8a26ebbca4ef
Alejandro <aleig.906@gmail.com>
2024-01-28 12:20:53 -0300

    created login service and connect w/ backend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { AuthserviceService } from 'src/app/services/auth/authservice.service';
+import { Router } from '@angular/router';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -16,15 +18,42 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class LoginComponent {
 
+  constructor(private authSer: AuthserviceService,
+              private router: Router) {}
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
-
   
   passwordFormControl = new FormControl('', [
     Validators.required,
   ]);
+
+  loginForm = new FormGroup({
+    emailFormControl: this.emailFormControl,
+    passwordFormControl: this.passwordFormControl
+  });
+
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      const credentials = {
+        adminUser: this.loginForm.value.emailFormControl || '',
+        adminPassword: this.loginForm.value.passwordFormControl || ''
+      }
+      this.authSer.authenticate(credentials)
+      .subscribe(
+        (response: any) => {
+          console.log('Login exitoso!', response)
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/admin']);
+        },
+        (error: any) => {
+          console.log(error)
+        }
+      )
+    }
+  }
 
   matcher = new MyErrorStateMatcher();
 
---
+++ src/app/services/auth/authservice.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import jwt_decode from 'jwt-decode';
+
+interface AuthResponse {
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthserviceService {
+
+  private authUrl = 'http://localhost:3000/admin/login'
+
+  constructor(private http: HttpClient) { }
+
+  public authenticate(credentials: {adminUser: string, adminPassword: string}) : Observable<AuthResponse> {
+    return this.http.post<any>(this.authUrl, credentials)
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = jwt_decode<any>(token);
+      const expirationDate = new Date(decodedToken.exp * 1000);
+      return expirationDate > new Date();
+    }
+    return false;
+  }
+}
